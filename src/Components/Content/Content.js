46d45f1bd47f9008edd80db2a2bc157ea97750f8
BartekLecki <leckibartek@gmail.com>
2020-06-29 02:28:25 +0200

    feat(list): MUI list skeleton

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import List from './List/List';
 
+import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -18,9 +18,7 @@ const Content = () => {
 
     return (
         <Container component="main" className={classes.main} maxWidth="sm">
-            <Typography variant="h3" component="h3" gutterBottom>
-                [ CONTENT ]
-            </Typography>
+            <List />
         </Container>
     )
 }
---
+++ src/Components/Content/List/ListHead/ListHead.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ListHead() {
+    return (
+        <div>
+            [LIST HEAD]
+        </div>
+    )
+}
+
+export default ListHead
