e5b4f2d8b3f2b3b4c176a533557621cbfee051ea
barteklecki <leckibartek@gmail.com>
2020-07-14 16:44:12 +0200

    refactor(MovieDetails): upgrade to ES2020 syntax for values defaults

--- src/Components/Content/MovieDetails/MovieDetails.js
+++ src/Components/Content/MovieDetails/MovieDetails.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { roundScore, printArray, roundRating } from '../../../utils';
-import { checkString, checkArray } from '../../../utils';
 import ReactHtmlParser from 'react-html-parser';
 
 import Card from '@material-ui/core/Card';
@@ -24,35 +23,69 @@ const MovieDetails = ({ classes, match }) => {
     if (!movieList.length) {
         return <Redirect to="/" />;
     }
+    
+    // show:
+    //     externals: {tvrage: null, thetvdb: 72097, imdb: "tt0050025"}
+    //     genres: Array(3)
+    //         0: "Action"
+    //         1: "Adventure"
+    //         2: "Western"
+    //         length: 3
+    //         __proto__: Array(0)
+    //     id: 3360
+    //     image:
+    //         medium: "http://static.tvmaze.com/uploads/images/medium_portrait/17/44293.jpg"
+    //         original: "http://static.tvmaze.com/uploads/images/original_untouched/17/44293.jpg"
+    //         __proto__: Object
+    //     language: "English"
+    //     name: "Have Gun, Will Travel"
+    //     network:
+    //         country: {name: "United States", code: "US", timezone: "America/New_York"}
+    //         id: 2
+    //         name: "CBS"
+    //         __proto__: Object
+    //     officialSite: null
+    //     premiered: "1957-09-14"
+    //     rating: {average: null}
+    //     runtime: 30
+    //     schedule:
+    //         days: ["Saturday"]
+    //         time: "21:30"
+    //         __proto__: Object
+    //     status: "Ended"
+    //     summary: "<p>The man known only as "Paladin" lives in San Francisco at the Carlton Hotel, accustomed to the fanciest food and furnishings. He afford it all by riding out as a gunslinger and charging top dollar for his services. His calling card? "Have Gun Will Travel." Paladin will take on any case if a client meets his fee, but whether he helps them or not depends on whether he thinks that they deserve it. A West Point graduate with an unknown past, Paladin is as likely to help a client with his lightning wits and sense of strategy, as he is to use his gun and his hold-out derringer.</p>"
+    //     type: "Scripted"
+    //     updated: 1580147281
+    //     url: "http://www.tvmaze.com/shows/3360/have-gun-will-travel"
+    //     webChannel: null
+    //     weight: 63
 
-    const {
-        params: { id },
-    } = match;
+    const { params: { id } } = match;
 
     const [
         {
             score,
             show: {
-                name = '-',
-                genres = ['-'],
+                name,
+                genres,
                 rating,
                 schedule,
-                runtime = 0,
-                premiered = '-',
-                status = '-',
-                image = '',
-                summary = '',
+                runtime,
+                premiered,
+                status,
+                image,
+                network,
+                summary,
             },
         },
     ] = findRecordById(movieList, id);
 
-    const ratingAverage = checkString(rating, 'average');
-    const days = checkArray(schedule, 'days');
-    const time = checkString(schedule, 'time');
-    const imageMedium = checkString(image, 'medium');
-    const imageUrl = imageMedium
-        ? imageMedium
-        : checkString(image, 'original');
+    const ratingAverage = rating?.average ?? ''; //ES2020
+    const days = schedule?.days ?? '';
+    const time = schedule?.time ?? '';
+    const imageUrl = image?.medium ?? image?.original ?? '';
+    const networkName = network?.name ?? '';
+    const networkCountry = network?.country?.name ?? '';
 
     return (
         <Card className={classes.root}>
@@ -102,12 +135,10 @@ const MovieDetails = ({ classes, match }) => {
                     variant="subtitle1"
                     color="textSecondary"
                 >
-                    <p>
-                        premiere:<strong> {premiered}</strong>
-                    </p>
-                    <p>
-                        status:<strong> {status}</strong>
-                    </p>
+                    { premiered ? (<p>premiere: <strong>{ premiered }</strong></p>) : null }
+                    { networkName ? (<p>network: <strong>{ networkName }</strong></p>) : null }
+                    { networkCountry ? (<p>country: <strong>{ networkCountry }</strong></p>) : null }
+                    { status ? (<p>status: <strong>{ status }</strong></p>) : null }
                 </Typography>
             </CardContent>
             <CardMedia
